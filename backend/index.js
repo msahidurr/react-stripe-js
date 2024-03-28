0f7455fa5f903a8dc3b8bf338978e232f8704c2f
ringlives <shohidurr49@gmail.com>
2024-03-28 14:17:38 +0600

    subscription

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 var cors = require("cors");
-const PORT = 3001;
+const PORT = 3004;
 
 const stripe = require("stripe")("sk_test_51Oqv91IsLSX0wXZBfBMh01oIDqdwKmZRMTOoD1V5AyDyseDSKX64vgRmvC0TYZppyZgqOesKihAInmtuoFJDhYdL00pywuCxag");
 
@@ -23,7 +23,43 @@ app.post("/create-payment-intent", async (req, res) => {
   });
 });
 
-app.get("/create-payment-intent", async (req, res) => {
+app.post('/create-subscription', async (req, res) => {
+
+  const { priceId, name, email, paymentMethod } = req.body;
+
+  const customer = await stripe.customers.create({
+    name: name,
+    email: email,
+    payment_method: paymentMethod,
+    invoice_settings: {
+      default_payment_method: paymentMethod,
+    },
+  });
+  
+  // create a stripe subscription
+  const subscription = await stripe.subscriptions.create({
+    customer: customer.id,
+    items: [{ price: priceId }],
+    payment_settings: {
+      payment_method_options: {
+        card: {
+          request_three_d_secure: 'any',
+        },
+      },
+      payment_method_types: ['card'],
+      save_default_payment_method: 'on_subscription',
+    },
+    expand: ['latest_invoice.payment_intent'],
+  });
+
+  // return the client secret and subscription id
+  res.send( {
+    clientSecret: subscription.latest_invoice.payment_intent.client_secret,
+    subscriptionId: subscription.id,
+  });
+})
+
+app.get("/test", async (req, res) => {
     res.send("test shohid");
   });
 
